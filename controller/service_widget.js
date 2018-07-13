8e9901b4f9a3ea81f8b82a66aab2fd16634e3edb
VALUETEAM\WangMa <marcowang05@gmail.com>
2018-07-13 10:28:10 +0200

    fixing and updating

--- controller/service_widget.js
+++ controller/service_widget.js
@@ -28,17 +28,15 @@ var c;
 
 	app.controller('widgetController',function($scope,servive1){
 		$scope.vect=[];
-		$scope.ind=0;
 $scope.api = 'https://openweathermap.org/data/2.5/weather?q=';
 $scope.key = '&appid=b6907d289e10d714a6e88b30761fae22';
 $scope.url='https://openweathermap.org/data/2.5/weather?q=rome&appid=b6907d289e10d714a6e88b30761fae22';
 	$scope.myFun = function(){
-				$scope.url=$scope.api+$scope.city+$scope.key;
+		$scope.url=$scope.api+$scope.city+$scope.key;
 		servive1.get($scope.url).then(function(data){
-			$scope.weather=data;
-			$scope.img='http://openweathermap.org/img/w/'+$scope.weather.weather[0].icon+'.png';
-				$scope.vect.push({city:$scope.weather, url:$scope.url, ind:$scope.ind, img:$scope.img});
-				$scope.ind=$scope.ind+1;
+	$scope.weather=data;
+	$scope.img='http://openweathermap.org/img/w/'+$scope.weather.weather[0].icon+'.png';
+	$scope.vect.push({city:$scope.weather, url:$scope.url, img:$scope.img});
 	$scope.vento="vento:";
 	$scope.umidita="umidità";
 	$scope.pressione="pressione:";
@@ -49,18 +47,14 @@ $scope.url='https://openweathermap.org/data/2.5/weather?q=rome&appid=b6907d289e1
 	$scope.weather.main.temp=$scope.weather.main.temp+'°C';
 	$scope.city="";
 		})
+
 		.catch(function(response){
+			alert($scope.city+' is not a city');
 			console.log(response.status);
 		})
+
 }
 	$scope.myChro = function(){ 
-
-
-
-
-
-
-
 
 		}
 	})
